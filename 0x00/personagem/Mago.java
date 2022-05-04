27a07eabafa0ca17fc96f47a8479ee62fd8624a0
yfreitasCIET <yfreitas@ciandt.com>
2022-05-04 16:00:51 -0300

    Resolvendo

--- 0x00/personagem/Mago.java
+++ 0x00/personagem/Mago.java
@@ -1,5 +1,5 @@
 class Mago extends Personagem{
-
+    
     public Mago(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
         super(nome, TipoPersonagem.MAGO, inteligencia, forca, vigor, resistencia, destreza);
 
